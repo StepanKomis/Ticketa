bb7af6f1025b0c8c3c3849ace913e89784b2d6ee
StepanKomis <komis.stepan@gmail.com>
2026-06-07 16:48:50 +0200

    docs: add custom index.html and swagger-initializer.js pointing to /docs/openapi.yaml

---
+++ src/www/docs/swagger-initializer.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  window.ui = SwaggerUIBundle({
+    url: "/docs/openapi.yaml",
+    dom_id: '#swagger-ui',
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "StandaloneLayout",
+    deepLinking: true,
+    persistAuthorization: true,
+  });
+};
